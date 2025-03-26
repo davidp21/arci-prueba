d33310680bfc7e9aaf459d863fb5a0e3a7b185e5
pcArci <davidapino00@gmail.com>
2025-03-26 15:10:56 -0400

    Creando componente User

---
+++ src/components/header/User.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center gap-3 bg-white p-2 rounded-lg hover:cursor-pointer">
+        <UserIcon class="size-6 sm:size-8 text-orange-500" />
+        <div class="hidden sm:flex flex-col items-center">
+            <span class="hidden lg:block font-medium text-sm">Mi carrito</span>
+            <span class="font-bold">$0.00</span>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+import { UserIcon } from '@heroicons/vue/24/solid';
+</script>
+
