2bba920c8fbc006e5ec0f16f3aa215e83eb5623d
pcArci <davidapino00@gmail.com>
2025-03-31 10:28:34 -0400

    fgh

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -5,12 +5,12 @@
   >
     <!-- Botón de cierre -->
     <button @click="$emit('close-menu')" class="p-4">✖ Cerrar</button>
-    <ul class="flex flex-col p-6 gap-4">
-      <li><router-link class="flex items-center gap-2"><Bars3Icon class="size-5" /> Categorías</router-link></li>
+    <ul class="flex flex-col p-6 gap-4 font-medium">
+      <li><router-link class="flex items-center gap-2"><Bars3Icon class="size-5" />Categorías</router-link></li>
       <li><router-link to="#">Inicio</router-link></li>
       <li><router-link to="#">Sobre nosotros</router-link></li>
-      <li class="flex items-center gap-1"><router-link to="#">Más Vendidos</router-link><ChevronDownIcon class="size-4" /></li>
-      <li><router-link to="#">Contacto</router-link></li>
+      <li class="flex items-center gap-1 hover:cursor-pointer"><router-link to="#">Más Vendidos</router-link><ChevronDownIcon class="size-4" /></li>
+      <li><router-link :to="{name:'contact'}">Contacto</router-link></li>
       <li class="flex items-center gap-2"><BanknotesIcon class="size-5" /><router-link to="#">Promociones</router-link></li>
     </ul>
   </div>
