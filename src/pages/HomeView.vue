c7a90ae42ad65dc11646d897e41d2b7427abaa78
pcArci <davidapino00@gmail.com>
2025-04-05 09:22:52 -0400

    cambio del fetch

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -4,9 +4,7 @@
     <Navbar/>
     <Hero/>
     <Categories/>
-    <button @click="crearUsuario" class="bg-blue-500 text-white font-medium cursor-pointer p-2 rounded-lg">
-      crear usuarui
-    </button>
+    
   </div>
 </template>
 
@@ -17,29 +15,7 @@ import Header from '@/components/header/Header.vue';
 import Navbar from '@/components/header/Navbar.vue';
 import Categories from '@/components/Products/Categories.vue';
 
-const crearUsuario = async () => {
-  try {
-    const response = await fetch("http://arci.com.ve/api-rest/create_user.php",{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        // nombre: nombre.value,//
-        // apellido: apellido.value,//
-        email: "david@gmail.com",
-        // cedula: cedula.value,
-        // contrasena: contrasena.value,
-        // direccion: direccion.value
-      })
-    });
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+
 </script>
 
 
